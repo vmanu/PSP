5613aeb80d1a31b8ec5f8e01a1428d8fc29cf329
vmanu <vmanuoveuh@hotmail.com>
2016-02-20 11:13:29 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ProyectoEmpresaVictorManuelOviedo/Index.php

---
+++ ObjetoPruebaObjetoVictorManuel/src/main/java/constantes/Constantes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constantes;
+
+/**
+ *
+ * @author dam2
+ */
+public class Constantes {
+
+    public static final String MENSAJE_TRUE = "TRUE";
+    public static final String MENSAJE_FALSE_REPETICION = "FALSE REPETICION";
+    public static final String MENSAJE_FALSE_REGISTRO = "FALSE REGISTRO";
+    public static final String MENSAJE_OK = "OK";
+    public static final String MENSAJE_WRONG = "WRONG";
+    public static final String OPERACION_LOGIN = "LOGIN";
+    public static final String OPERACION_REGISTRO = "REGISTRO";
+}
